83b38eabc0fa49c4b1a4d822d1c0f05ffb91af87
Marcin Radoszewski <moriturius@gmail.com>
2026-06-28 06:57:02 +0200

    Add PWA support with Jack Russell terrier icon

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'dogfood-v1';
+const ASSETS = ['./', './foods.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+);
+
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
